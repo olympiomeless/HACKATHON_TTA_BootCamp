3fcd60a62ebb14461ec0ff11b0b29a9ee309c446
olympiomeless <olympiomeless@gmail.com>
2026-06-08 12:08:11 +0000

    Delete Transaction.py

--- Transaction.py
+++
@@ -1,9 +0,0 @@
-class Transaction :
-    def __init__(self, amount, date, description):
-        self.amount = amount
-        self.date = date
-        self.description = description
-
-    def __str__(self):
-        return f"Transaction(amount={self.amount}, date={self.date}, description={self.description})"
-    
