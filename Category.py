e6607c132527023ccb3d7c7f7e337fe06b13af2a
olympiomeless <olympiomeless@gmail.com>
2026-06-08 12:07:49 +0000

    Delete Category.py

--- Category.py
+++
@@ -1,8 +0,0 @@
-class Category :
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-
-    def __str__(self):
-        return f"Category(name={self.name}, description={self.description})"
-    
